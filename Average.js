278c778fa771e144749c2c9e06913901637ade8c
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-07 23:00:29 -0400

    add Average.js

---
+++ Average.js
@@ -0,0 +1,14 @@
+//I implement and export by default a function that returns 
+// the arithmetic mean of all passed arguments. 
+// If no arguments are passed to the function, then it should return null.
+
+export default function average (...numbers)  {
+    const getSumOfNumbers = _.sum(numbers);
+    console.log(getSumOfNumbers);
+    const length = numbers.length;
+      if (numbers.length === 0) {
+      return null;
+    } else {
+    return getSumOfNumbers/length;
+    }
+    }
