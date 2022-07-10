f5a4cbc5e4908ea6fec87013a0ed9f6570722540
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-09 23:18:38 -0400

    update Spread

---
+++ Functions/Spread.js
@@ -0,0 +1,8 @@
+// a function that converts dates into an array 
+// of human-readable English strings. 
+// Each of the dates is represented 
+// by the array [2001, 10, 18], where the first element is the year, 
+// the second is the month, and the third is the number.
+//  The input function must take any number of parameters. 
+// If nothing was passed to the function, it should return an empty array. 
+// Export the default function.
