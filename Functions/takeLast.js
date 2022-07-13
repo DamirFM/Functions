009cda3eb14cf9cdf344a0eb3bb52a6ab4c38ed9
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-12 23:32:04 -0400

    add takeLast

---
+++ Functions/takeLast.js
@@ -0,0 +1,4 @@
+//the takeLast() function, which returns the last n characters of a string in reverse order. 
+// The number of characters is passed to takeLast() as the second parameter. 
+// If an empty string or a string less than the required length is passed, the function should return null.
+
