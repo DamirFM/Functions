8a1a0d370ea6b37b7cd6dffaa8b8d9368bd5aec7
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-12 23:32:42 -0400

    add takeLast.js

--- Functions/takeLast.js
+++ Functions/takeLast.js
@@ -2,3 +2,23 @@
 // The number of characters is passed to takeLast() as the second parameter. 
 // If an empty string or a string less than the required length is passed, the function should return null.
 
+const run = (text) => {
+ 
+    const takeLast = (str, n) => {
+  if (str.length === 0 || str.length < n) {
+    return null;
+    } 
+     // let i = str.length - 1;
+      let result = '';
+      for (let i = str.length - 1; i >= str.length - n; i --) {
+        result = result + str[i];
+      }
+  console.log(result);
+      return result;
+    }
+    
+  
+    return takeLast(text, 4);
+  };
+  
+  export default run;
