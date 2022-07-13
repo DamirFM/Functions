4f20658f5cb9f001eb292a95257d459c2333b16f
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-13 09:31:17 -0400

    add simpleOfSort

---
+++ Functions/simpleOfSort.js
@@ -0,0 +1,26 @@
+// https://ru.hexlet.io/courses/js-functions/lessons/high-order-functions/theory_unit
+
+
+const users = [
+    { name: 'Igor', age: 19 },
+    { name: 'Danil', age: 1 },
+    { name: 'Vovan', age: 4 },
+    { name: 'Matvey', age: 16 },
+  ];
+  
+  // Функция принимает на вход сравниваемые элементы массива
+  const compare = (a, b) => {
+    if (a.age === b.age) {
+      return 0;
+    }
+  
+    return a.age > b.age ? 1 : -1;
+  };
+  
+  users.sort(compare);
+  
+  console.log(users);
+  // => [ { name: 'Danil', age: 1 },
+  //      { name: 'Vovan', age: 4 },
+  //      { name: 'Matvey', age: 16 },
+  //      { name: 'Igor', age: 19 } ]
