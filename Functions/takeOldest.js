e06163815b781936249728e80855f40cd5d0072c
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-14 00:17:14 -0400

    add takeOldest.js

--- Functions/takeOldest.js
+++ Functions/takeOldest.js
@@ -1,3 +1,6 @@
+//the takeOldest() function, which takes a list of users as input and returns the oldest ones. 
+// The number of returned users is set by the second parameter, which is equal to one by default. 
+// Export this function by default.
 
 const users = [
     { name: 'Tirion', birthday: 'Nov 19, 1988' },
@@ -12,5 +15,5 @@ const users = [
   export default function takeOldest (users, num = 1) {
     const date = (users) => Date.parse(users.birthday)
     const sorted = _.sortBy(users, date);
-    console.log(sorted)
+    return sorted.slice(0, num);
   }
