a8ae500111adca37fc09888f647314b08d96878d
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-16 22:47:14 -0400

    add getChildren.js

--- Functions/getChildren.js
+++ Functions/getChildren.js
@@ -1,8 +1,8 @@
+// a function that takes a list of users as input and returns a flat list of their children. 
+// The children of each user are stored as an array in the children key.
+
 export default function getChildren (users) {
-    const childrenOfUsers = users.map(({ children }) => children);
-   return childrenOfUsers.flat();
-  
-  
-  
+ const childrenNames = users.map(({children}) => children);
+ return childrenNames.flat();
   
   }
