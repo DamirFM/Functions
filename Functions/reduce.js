81c16c49eb45cefeec085d0354bd91268bfa071e
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-20 22:29:28 -0400

    add reduce

--- Functions/reduce.js
+++ Functions/reduce.js
@@ -1,13 +1,12 @@
-const groupBy = (objects, key) => objects.reduce((acc, object) => {
-
-    const groupName = object[key];
-    // контейнером группы выступает массив
-    // Оператор нулевого слияния возвращает пустой массив, если в аккумуляторе ничего нет
-    const group = acc[groupName] ?? [];
-    // возвращается новый объект аккумулятора
-    // старый аккумулятор деструктурируется, для текущей группы записывается новый массив с данными
-    // квадратные скобки нужны, чтобы указать имя группы в качестве ключа
-    return { ...acc, [groupName]: group.concat(object) };
-  }, {});
-  
-  export default groupBy;
+export default function groupBy (students, property) {
+    const acc = {};
+    for (const student of students) {
+     
+      if (!Object.hasOwn(acc, student[property])) {
+        console.log(acc)
+        acc[student[property]] = [];
+      }
+      acc[student[property]].push(student);
+    }
+    return acc;
+  } 
