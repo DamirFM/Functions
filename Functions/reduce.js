32f30e7c8cca35d8c7c040b3337ed1aed56b88ae
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-21 22:44:31 -0400

    add reduce

--- Functions/reduce.js
+++ Functions/reduce.js
@@ -1,12 +1,16 @@
-export default function groupBy (students, property) {
-    const acc = {};
-    for (const student of students) {
-     
-      if (!Object.hasOwn(acc, student[property])) {
-        console.log(acc)
-        acc[student[property]] = [];
-      }
-      acc[student[property]].push(student);
+//a function for grouping objects by a given property. 
+// The function takes as arguments an array of objects and the name of the property to be grouped. 
+// It should return an object where the key is the value for the given property and the value is 
+// an array with data appropriate for the group.
+
+
+export default function groupBy (students, properties) {
+  const result = students.reduce((acc, student) => {
+    if (!Object.hasOwn(acc, student[properties])) {
+      acc[student[properties]] = [];
     }
+    acc[student[properties]].push(student);
     return acc;
-  } 
+  }, {})
+  return result;
+}
