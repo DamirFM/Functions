438ac1f3be1fa9bd4fcd05544c26869b8e3fab5d
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-25 19:58:30 -0400

    add domains.js

--- Functions/domains.js
+++ Functions/domains.js
@@ -1,12 +1,36 @@
-const getFreeDomainsCount = (emails) => emails
-  .map((email) => {
-    const [, domain] = email.split('@');
-    return domain;
-  })
-  .filter((domain) => freeEmailDomains.includes(domain))
-  .reduce((acc, domain) => {
-    const count = get(acc, domain, 0) + 1;
-    return { ...acc, [domain]: count };
-  }, {});
+// a function that takes a list of emails as input and returns
+//  the number of emails located on each free domain. 
+// The list of free domains is stored in the freeEmailDomains constant.
 
-export default getFreeDomainsCount;
+const emails = [
+    'info@gmail.com',
+    'info@yandex.ru',
+    'info@hotmail.com',
+    'mk@host.com',
+    'support@hexlet.io',
+    'key@yandex.ru',
+    'sergey@gmail.com',
+    'vovan@gmail.com',
+    'vovan@hotmail.com',
+    'ivan@yahoo.com',
+  ];
+
+  const freeEmailDomains = [
+    'gmail.com',
+    'yandex.ru',
+    'hotmail.com',
+    'yahoo.com',
+  ];
+
+export default function (emails) {
+    const mappedEmails = emails.map((email) => email.split('@')[1]);
+    const filtredEmails = mappedEmails.filter((domain) => freeEmailDomains.includes(domain));
+    const reducedEmails = filtredEmails.reduce((acc, domain) => {
+        if (!acc[domain]){
+            acc[domain] = 0;
+        }
+        acc[domain] += 1;
+        return acc;
+    }, {})
+    return reducedEmails;
+}
