187495d82fb64122c0df1715bcf8de74c429af71
Damir Yakupov <yakupovdamir93@gmail.com>
2022-08-02 09:39:33 -0400

    add reduce_one.js;

--- Functions/reduce_one.js
+++ Functions/reduce_one.js
@@ -6,4 +6,45 @@ const average = (numbers) => {
     return sum / length;
 };
 
-console.log(average);
+console.log(average(numbers));
+
+const users = [{
+    username: "lando12",
+    jedi: false
+}, {
+    username: "kvaigon99",
+    jedi: true
+}, {
+    username: "anakin skywalker",
+    jedi: false // DARK SIDE ISN'T JEDI WAY!!!
+}, {
+    username: "littlegreenpug",
+    jedi: true
+}, {
+    username: "ray",
+    jedi: true
+}, {
+    username: "padmebestqueenever",
+    jedi: false
+}];
+
+const getJedi = (users) =>{
+    return users.reduce((acc, user) => {
+        if (user.jedi) {
+            acc += 1;
+        }
+        return acc;
+    } , 0)   
+}
+console.log(getJedi(users));
+
+const getNames = (users) => {
+    return users.reduce((arr, item) => {
+        if (item.jedi) {
+            arr.push(item[username])
+        }
+        return arr;
+    } , [])
+}
+
+console.log(getNames(users))
