78794ea40f42aae1ca6f53fe32195dc426e017a5
Damir Yakupov <yakupovdamir93@gmail.com>
2022-12-25 16:29:14 -0500

    add list.js

---
+++ Functions/list.js
@@ -0,0 +1,16 @@
+const flatten = (list) => list.reduce((acc, element) => {
+    const result = (Array.isArray(element) ? [...acc, ...flatten(element)] : [...acc, element]);
+    return result;
+  }, []);
+  
+  export default flatten;
+//Реализуйте и экспортируйте по умолчанию функцию, которая делает плоским вложенный массив.
+
+//Для решения задачи нельзя использовать готовые методы для выравнивания массивов.
+
+//Примеры
+//const list = [1, 2, [3, 5], [[4, 3], 2]];
+
+//flatten(list); // [1, 2, 3, 5, 4, 3, 2]
+//Подсказки
+//Array.isArray - проверяет, является ли элемент массивом.
