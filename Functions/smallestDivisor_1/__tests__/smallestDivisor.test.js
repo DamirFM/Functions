1ccf094ee1bfed31bc1da0ef3e33b90dbceef4e3
Damir Yakupov <yakupovdamir93@gmail.com>
2023-10-16 20:33:36 -0400

    add while loop

---
+++ Functions/smallestDivisor_1/__tests__/smallestDivisor.test.js
@@ -0,0 +1,14 @@
+import smallestDivisor from '../solution.js';
+
+test('solution', () => {
+  expect(smallestDivisor(-3)).toEqual(NaN);
+  expect(smallestDivisor(0)).toEqual(NaN);
+  expect(smallestDivisor(1)).toBe(1);
+  expect(smallestDivisor(4)).toBe(2);
+  expect(smallestDivisor(121)).toBe(11);
+  expect(smallestDivisor(3)).toBe(3);
+  expect(smallestDivisor(8)).toBe(2);
+  expect(smallestDivisor(9)).toBe(3);
+  expect(smallestDivisor(17)).toBe(17);
+  expect(smallestDivisor(15)).toBe(3);
+});
---
+++ Functions/smallestDivisor_1/smallestDivisor.js
@@ -0,0 +1,24 @@
+// You must find the smallest integer divisor of a number. 
+// The behavior of the function is the same as in the previous lesson, 
+// but the implementation (function code) must be different. 
+// This time implement an imperative iterative process, which means:
+
+// don't use recursion
+// use variables
+// use a while loop
+// For example, the smallest divisor of 15 is 3.
+
+const smallestDivisor = (num) => {
+    if (num < 1) {
+        return NaN;
+    }
+    if (num === 1) {
+        return 1;
+    }
+    let result = 2;
+    while (num % result !== 0) {
+        result = result + 1;
+    }
+    return result;
+}
+export default smallestDivisor;
