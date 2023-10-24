1d091c71cdda3389d7ca8d263e8948875b839d1e
Damir Yakupov <yakupovdamir93@gmail.com>
2023-10-23 22:31:35 -0400

    add isPrime code and tests

---
+++ Functions/isPrime/__tests__/isPrime.test.js
@@ -0,0 +1,20 @@
+import isPrime from '../isPrime.js';
+
+test('not prime', () => {
+  expect(isPrime(-3)).toBe(false);
+  expect(isPrime(0)).toBe(false);
+  expect(isPrime(-1)).toBe(false);
+  expect(isPrime(1)).toBe(false);
+  expect(isPrime(4)).toBe(false);
+  expect(isPrime(21)).toBe(false);
+  expect(isPrime(35)).toBe(false);
+  expect(isPrime(-10)).toBe(false);
+});
+
+test('prime', () => {
+  expect(isPrime(2)).toBe(true);
+  
+  expect(isPrime(3)).toBe(true);
+  
+  expect(isPrime(23)).toBe(true);
+});
